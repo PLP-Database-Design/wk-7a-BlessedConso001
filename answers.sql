4dc87142949fcc7510a82eb9c0b9c31780477edd
Blessed001 <mugoconsolata70@gmail.com>
2025-05-04 12:14:20 +0300

    corrected wk 7 database assignment

--- answers.sql
+++ answers.sql
@@ -2,40 +2,22 @@
 -- **************************************************************************************************
 
 -- CREATING TABLES
--- CREATING TABLE 1
-CREATE DATABASE IF NOT EXISTS WEEK7;
-USE WEEK7;
-CREATE TABLE CustomerDetails (
-    OrderID INT PRIMARY KEY,
-    Customer_Name VARCHAR(100)
-);
-
-
--- CREATING TABLE 2
-CREATE TABLE productDetail (
-    ProductID INT PRIMARY KEY AUTO_INCREMENT,
+--- CREATING TABLE 1
+    CREATE TABLE productDetail (
     OrderID INT,
-    Products VARCHAR(100),
-    FOREIGN KEY (OrderID) REFERENCES CustomerDetails(OrderID)
-);
+    Customer_Name VARCHAR(100)
+    Products VARCHAR(100));
+   
 
-
--- INSERTING VALUES TO TABLE 1
-INSERT INTO CustomerDetails (OrderID, Customer_Name)
-VALUES 
-    (101, 'John Doe'), 
-    (102, 'Jane Smith'), 
-    (103, 'Emily Clark');
-
--- INSERTING VALUES TO TABLE 2
-INSERT INTO productDetail(orderID, Products)
-VALUES 
-    (101, 'Laptop'), 
-    (101, 'Mouse'), 
-    (102, 'Tablet'), 
-    (102, 'Keyboard'),
-    (102, 'Mouse'), 
-    (103, 'Phone');
+--- INSERTING VALUES TO TABLE 1
+    INSERT INTO productDetail(orderID,CustomerName, Products)
+    VALUES 
+    (101,'John Doe', 'Laptop'), 
+    (101, 'John Doe','Mouse'), 
+    (102, 'Jane Smith','Tablet'), 
+    (102, 'Jane Smith','Keyboard'),
+    (102, 'Jane Smith','Mouse'), 
+    (103, 'Emily Clark','Phone');
 
 
 
@@ -43,32 +25,32 @@ VALUES
 --- QUESTION 2
 --- ******************************************************************************************************
 
---- CREATING TABLE 1
+-- CREATING TABLE 1
 CREATE TABLE CustomerDetail(
     OrderID INT PRIMARY KEY,
     CustomerName VARCHAR(100)
 );
 
---- CREATING TABLE 2
-CREATE TABLE ProductName (
-    ProductID INT PRIMARY KEY AUTO_INCREMENT,
-    OrderID INT,
-    Product VARCHAR(100),
-    Quantity INT,
-    FOREIGN KEY (OrderID) REFERENCES CustomerDetail(OrderID));
-
-
---- INSERTING VAUES TO TABLE 1
-INSERT INTO CustomerDetail(OrderID, CustomerName)
-VALUES 
+-- INSERTING VAUES TO TABLE 1
+    INSERT INTO CustomerDetail(OrderID, CustomerName)
+    VALUES 
     (101, 'John Doe'),  
     (102, 'Jane Smith'), 
     (103, 'Emily Clark');
 
 
---- INSERTING VALUES TO TABLE 2
-INSERT INTO ProductName(OrderID, Product, Quantity)
-VALUES 
+-- CREATING TABLE 2
+  CREATE TABLE ProductName (
+    OrderID INT,
+    Product VARCHAR(100),
+    Quantity INT,
+    PRIMARY KEY (Product, OrderID),
+    FOREIGN KEY (OrderID) REFERENCES CustomerDetail(OrderID));
+
+
+--  INSERTING VALUES TO TABLE 2
+    INSERT INTO ProductName(OrderID, Product, Quantity)
+    VALUES 
     (101, 'Laptop', 2),  
     (101, 'Mouse', 1), 
     (102, 'Tablet', 3), 
